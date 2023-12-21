79f61250e237f9557587e0e0c7d25a5e35186e14
jckhe <iamjckhe@gmail.com>
2023-12-21 09:11:40 -0800

    Switched to coinbase API

--- stocks.py
+++ stocks.py
@@ -14,27 +14,6 @@ trading_day = get_weekday_or_previous_friday('America/Los_Angeles')
 def polygon_query(ticker):
     return f"https://api.polygon.io/v1/open-close/{ticker}/{trading_day}?adjusted=true&apiKey={polygon_token}"
 
-# def stockFetcher(message):
-#   price = finnhub_client.quote(message)
-#   output = (
-#     hikari.Embed(title=f"{message} price", description='', color="#FF0000")
-#     .add_field(name="Current Price: ", value=f"${price['c']}", inline=True)
-#     .add_field(name="Change", value=price['d'], inline=True)
-#     .add_field(name="% Change", value=price['dp'], inline=True)
-#     .set_footer(text='yammy', icon='https://cdn-icons-png.flaticon.com/512/6699/6699255.png')
-#     )
-#   return output
-
-# async def stockFetcher(stocks, ctx):
-#     for stock in stocks:
-#         embed = hikari.Embed(title=f"{stock} Price", description='', color="#FF0000")
-#         price = finnhub_client.quote(stock)
-#         embed.add_field(name="Current Price", value=f"${price['c']}", inline=True)
-#         embed.add_field(name="Change", value=price['d'], inline=True)
-#         embed.add_field(name="% Change", value=price['dp'], inline=True)
-#         embed.set_footer(text='yammy', icon='https://cdn-icons-png.flaticon.com/512/6699/6699255.png')
-
-#         await ctx.respond(embed=embed)
 def stockFetcher(stock, message):
     embed = hikari.Embed(title=f"{stock} Price", description='', color="#FF0000")
     price = finnhub_client.quote(stock)
@@ -50,6 +29,5 @@ def afterHoursFetcher(stock, message):
     embed = stockFetcher(stock, message)
     polygon_fetch = requests.get(polygon_query(stock))
     polygon_response = json.loads(polygon_fetch.text)
-    print(polygon_response)
     embed.add_field(name=f"After Hours ({trading_day})", value=f"{round(polygon_response['afterHours'], 2)}", inline=False)
     return embed
